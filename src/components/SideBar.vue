31c174643a1790c76f1925ae3eb95136facc0ebc
Ttst <thibautstachnick@gmail.com>
2024-04-07 16:08:57 +0200

    add sidebar component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="sidebar">
+  <span class="name">
+    <img class="logo" src="../../public/assets/logo.svg" alt="logo">
+    <span>nomProvisoire</span>
+  </span>
+</div>
+</template>
+
+<style scoped lang="scss">
+.sidebar{
+  border-top-left-radius: 25px;
+  border-bottom-left-radius: 25px;
+  padding-top: 5vh;
+  width: 25vw;
+  height: 100vh;
+  background-color: blue;
+}
+
+.logo{
+  width: 5vh;
+  fill: white;
+}
+
+.name{
+  color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+</style>
